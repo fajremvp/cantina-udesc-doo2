527a8573e4a9bccbad59a5ef4df25c9c8c8e47fa
Fajre <105254444+fajremvp@users.noreply.github.com>
2026-06-27 17:16:02 -0300

    chore: setup database schema and connection infrastructure

--- src/main/java/br/udesc/doo2/cantina/infra/Conexao.java
+++ src/main/java/br/udesc/doo2/cantina/infra/Conexao.java
@@ -1,5 +1,27 @@
 package br.udesc.doo2.cantina.infra;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Conexao {
-    
+
+	private static EntityManagerFactory emf;
+
+	// Construtor privado para impedir instanciamento (Padrão Singleton)
+	private Conexao() {
+	}
+
+	public static EntityManager getEntityManager() {
+		if (emf == null) {
+			emf = Persistence.createEntityManagerFactory("cantinaPU");
+		}
+		return emf.createEntityManager();
+	}
+
+	public static void fecharConexao() {
+		if (emf != null && emf.isOpen()) {
+			emf.close();
+		}
+	}
 }

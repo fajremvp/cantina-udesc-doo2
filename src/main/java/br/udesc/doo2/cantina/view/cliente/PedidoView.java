f557a14aaf99874d8f067b4bf69f7ed3bb4e8cdf
maria-zortea <dudazzortea@gmail.com>
2026-06-29 17:44:44 -0300

    Criação tela PedidoView

--- src/main/java/br/udesc/doo2/cantina/view/cliente/PedidoView.java
+++ src/main/java/br/udesc/doo2/cantina/view/cliente/PedidoView.java
@@ -37,7 +37,7 @@ public class PedidoView extends javax.swing.JFrame {
         rbtnCarne2Pedido = new javax.swing.JRadioButton();
         rbtnOpcaoConsumoLevar = new javax.swing.JRadioButton();
         rbtnOpcaoConsumoLocal = new javax.swing.JRadioButton();
-        jButton1 = new javax.swing.JButton();
+        btnFinalizarPedido = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -60,7 +60,7 @@ public class PedidoView extends javax.swing.JFrame {
         btngroupOpcaoConsumo.add(rbtnOpcaoConsumoLocal);
         rbtnOpcaoConsumoLocal.setText("Local");
 
-        jButton1.setText("Salvar Pedido");
+        btnFinalizarPedido.setText("Finalizar Pedido");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -73,7 +73,7 @@ public class PedidoView extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(53, 53, 53)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnFinalizarPedido, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(rbtnOpcaoConsumoLevar)
@@ -104,7 +104,7 @@ public class PedidoView extends javax.swing.JFrame {
                     .addComponent(rbtnOpcaoConsumoLevar)
                     .addComponent(rbtnOpcaoConsumoLocal))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addComponent(btnFinalizarPedido)
                 .addGap(18, 18, 18))
         );
 
@@ -137,9 +137,9 @@ public class PedidoView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnFinalizarPedido;
     private javax.swing.ButtonGroup btngroupOpcaoCarne;
     private javax.swing.ButtonGroup btngroupOpcaoConsumo;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;

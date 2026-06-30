05968b8aecf0323469587d746d8084542ceba138
Gustavo Hoffmann <157737954+GustavoHoffmann3@users.noreply.github.com>
2026-06-30 19:44:09 -0300

    Update CadastrarRefeicaoView.java

--- src/main/java/br/udesc/doo2/cantina/view/administrador/CadastrarRefeicaoView.java
+++ src/main/java/br/udesc/doo2/cantina/view/administrador/CadastrarRefeicaoView.java
@@ -57,12 +57,8 @@ public class CadastrarRefeicaoView extends javax.swing.JFrame {
         return (String) cbCarne1.getSelectedItem();
     }
     
-    public String getCarne2Selecionada() throws RefeicaoException {
-<<<<<<< HEAD
-        if (cbCarne1.getSelectedItem().equals(cbCarne2.getSelectedItem())) {
-=======
+    public String getCarne2Selecionada() throws RefeicaoException 
         if (cbCarne2.getSelectedItem().equals(cbCarne1.getSelectedItem())) {
->>>>>>> a1c70a8ab44cc51431b8d2687bb44d53dcdfd63b
             throw new RefeicaoException("As opções de carne não podem ser iguais");
         }
         return (String) cbCarne2.getSelectedItem();

3b6c9e7f74a5c9c9b143f4b01c3deafa7f03fd96
Fajre <105254444+fajremvp@users.noreply.github.com>
2026-07-01 00:26:16 -0300

    feat: add database seed and setup instructions

---
+++ database/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO usuario (tipo_usuario, id, nome, senha, matricula)
+VALUES (
+    'ADMINISTRADOR',
+    999,
+    'Verônica',
+    '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9',
+    NULL
+);

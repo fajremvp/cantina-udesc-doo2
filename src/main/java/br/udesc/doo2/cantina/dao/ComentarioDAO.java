e1633c43016a41c77520e5cf00603a92d53217a5
Ruan Kestring <ruancarkestring@gmail.com>
2026-07-06 20:52:59 -0300

    feat: adicionado os metodos de persistencia de comentarios

--- src/main/java/br/udesc/doo2/cantina/dao/ComentarioDAO.java
+++ src/main/java/br/udesc/doo2/cantina/dao/ComentarioDAO.java
@@ -1,5 +1,41 @@
 package br.udesc.doo2.cantina.dao;
 
-public class ComentarioDAO {
+import br.udesc.doo2.cantina.infra.Conexao;
+import br.udesc.doo2.cantina.model.Comentario;
+import br.udesc.doo2.cantina.repository.ComentarioRepository;
+import java.util.List;
+import javax.persistence.EntityManager;
+
+public class ComentarioDAO implements ComentarioRepository {
+
+    @Override
+    public void salvar(Comentario comentario) {
+        EntityManager em = Conexao.getEntityManager();
+        
+        try {
+            em.getTransaction().begin();
+            em.persist(comentario);
+            em.getTransaction().commit();
+        } catch(RuntimeException e) {
+            if(em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Comentario> buscarTodos() {
+        EntityManager em = Conexao.getEntityManager();
+        
+        try {
+            return em.createQuery(
+                "SELECT * FROM comentario"
+            ).getResultList();
+        } finally {
+            em.close();
+        }
+    }
     
 }
